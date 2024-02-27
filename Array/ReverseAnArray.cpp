b285daccdee338b2b885b5c8bd84c11cb22f8737
Sujan Maji <dev.sujanmaji@gmail.com>
2024-02-27 13:09:00 +0530

    Reverse an Array

---
+++ Array/ReverseAnArray.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void reverseArray(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    printArray(arr, size);
+    return 0;
+}
