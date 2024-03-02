179886f74755d7b1fd3ebd519a44296cfc5139b8
Sujan Maji <dev.sujanmaji@gmail.com>
2024-03-02 18:12:58 +0530

    Recursive Bubble Sort

---
+++ Recursion/BubbleSort.cpp
@@ -0,0 +1,52 @@
+/**
+ * Bubble Sort Using Recursion
+ * Author: Sujan Maji
+ * Date: 27/02/2024
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    if (n < 2)
+        return;
+
+    bool swapped = false;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+            swapped = true;
+        }
+    }
+
+    if (swapped == false)
+        return;
+
+    bubbleSort(arr, n - 1);
+}
+
+int main()
+{
+    // Write your code
+    int arr[] = {5, 3, 8, 4, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, size);
+
+    printArray(arr, size);
+
+    return 0;
+}
